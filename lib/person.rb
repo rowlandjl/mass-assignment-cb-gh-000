1dfe141b5318439aad016ecb194d1fb55ad78a7c
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-29 02:19:33 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,9 @@ class Person
     :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end 
   end
 
 end
